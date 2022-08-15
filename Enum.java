666b8d162bd0c8d129526a9d532c918c43a761b1
Rukku22 <ruksana.syed246@gmail.com>
2022-08-16 01:27:00 +0530

    'java_ex_files'

---
+++ Enum.java
@@ -0,0 +1,12 @@
+package project;
+
+public class Enum {
+	public	enum Months { JAN,FEB, MARCH,APR,MAY,JUNE,JULY,AUG,SET,OCT } 
+	 
+	public static void main(String[] args) {  
+	 
+	for (Months s : Months.values())  
+	System.out.println(s);  
+	}}  
+
+
